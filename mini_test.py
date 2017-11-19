2e2d4026f152356a1e1f5f34d6732bf4e027a8db
Yuxing Chen <yxchen2828@gmail.com>
2017-11-18 23:40:07 -0800

    mini_test: write info to a text file
    pitch, start_time, end_time, time_difference, and score-relative note length

--- mini_test.py
+++ mini_test.py
@@ -2,25 +2,24 @@ import tensorflow as tf
 from magenta.music import note_sequence_io
 from magenta.protobuf import music_pb2
 
-'''
-sequences = []
-for i in range(4):
-    sequence = music_pb2.NoteSequence()
-    sequence.id = str(i)
-    sequence.notes.add().pitch = i
-    sequences.append(sequence)
+'''                                                                                                                                                                                                                                        
+sequences = []                                                                                                                                                                                                                             
+for i in range(4):                                                                                                                                                                                                                         
+    sequence = music_pb2.NoteSequence()                                                                                                                                                                                                    
+    sequence.id = str(i)                                                                                                                                                                                                                   
+    sequence.notes.add().pitch = i                                                                                                                                                                                                         
+    sequences.append(sequence)                                                                                                                                                                                                             
 '''
 
 
 def main():
+    fp = open("pitch.txt","w+")
+    fp.write('pitch,start_time,end_time,time_diff,note_len\n')
     file_name = "notes.tfrecord"
     sequence = music_pb2.NoteSequence()
     for i, sequence in enumerate(note_sequence_io.note_sequence_record_iterator(file_name)):
-        print(i)
-        print(sequence.id)
-        print(sequence.filename)
-        for note in sequence.notes:
-            print(note.pitch)
-
+	for note in sequence.notes:
+            fp.write('{},{},{},{},{}/{}\n'.format(note.pitch,note.start_time,note.end_time,note.end_time - note.start_time,note.numerator,note.denominator))
+	
 if __name__ == "__main__":
     main()
