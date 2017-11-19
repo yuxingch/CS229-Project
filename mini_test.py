4d9e7671f5420430ac55b2066fe58e84e2313c27
Yuxing Chen <yxchen2828@gmail.com>
2017-11-19 01:28:11 -0800

    write to tempo.txt and pitch.txt

--- mini_test.py
+++ mini_test.py
@@ -2,24 +2,28 @@ import tensorflow as tf
 from magenta.music import note_sequence_io
 from magenta.protobuf import music_pb2
 
-'''                                                                                                                                                                                                                                        
-sequences = []                                                                                                                                                                                                                             
-for i in range(4):                                                                                                                                                                                                                         
-    sequence = music_pb2.NoteSequence()                                                                                                                                                                                                    
-    sequence.id = str(i)                                                                                                                                                                                                                   
-    sequence.notes.add().pitch = i                                                                                                                                                                                                         
-    sequences.append(sequence)                                                                                                                                                                                                             
+'''                                                                                                                                                                                      
+sequences = []                                                                                                                                                                           
+for i in range(4):                                                                                                                                                                       
+    sequence = music_pb2.NoteSequence()                                                                                                                                                  
+    sequence.id = str(i)                                                                                                                                                                 
+    sequence.notes.add().pitch = i                                                                                                                                                       
+    sequences.append(sequence)                                                                                                                                                           
 '''
 
 
 def main():
     fp = open("pitch.txt","w+")
+    ft = open("tempo.txt","w+")
     fp.write('pitch,start_time,end_time,time_diff,note_len\n')
     file_name = "notes.tfrecord"
     sequence = music_pb2.NoteSequence()
     for i, sequence in enumerate(note_sequence_io.note_sequence_record_iterator(file_name)):
-	for note in sequence.notes:
+        for tempo in sequence.tempos:
+            ft.write('time:{}\n'.format(tempo.time))
+            ft.write('qpm:{}\n'.format(tempo.qpm))
+        for note in sequence.notes:
             fp.write('{},{},{},{},{}/{}\n'.format(note.pitch,note.start_time,note.end_time,note.end_time - note.start_time,note.numerator,note.denominator))
-	
+
 if __name__ == "__main__":
     main()
