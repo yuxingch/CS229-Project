78c564ee1bae071c6f68391ba944be23e2a135c0
EmmaYChen <yaochen.jo@gmail.com>
2017-12-15 16:50:10 -0700

    delete unused code, add some comments

--- code/rnn_main.py
+++ code/rnn_main.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 from rnn_model import RnnModel
 
 import os
-
 import numpy as np
 from numpy import linalg as LA
 import tensorflow as tf
@@ -69,75 +68,30 @@ def main(argv=None):
     dev_set = dev_matrix[:,:,:]
     dev_input = dev_set
     
-    # 
-    # tranpose the matrix, now its dimension is timestep*note_size
-    #channelMatrix  = np.transpose(channelMatrix )
-    #print(channelMatrix.shape)
-    
-    #expand dimension, now batch_size*time*note_size
-    #channelMatrix = np.expand_dims(channelMatrix , axis=0)
-    #print(channelMatrix.shape)
-    
-    #expand note_size from 5*1 to 128*1
-    # music_input = matrix_to_input(channelMatrix)
-    #print(music_input.shape)
-    #print((music_input.shape)[0])
-    #print((music_input.shape)[1])
-    #print((music_input.shape)[2])
-    
+    # parameters initialization
+    num_epoch = 10
+    step = 0
     seq_len = 50
     placeholder = tensorflow_music_input(music_input, seq_len)
     model = RnnModel(placeholder) 
     config = tf.ConfigProto(log_device_placement=False)
     
-    
-
     # saver
     saver = tf.train.Saver()
-
-    # save_dir = '/Users/yuxing/Desktop/Stanford/Academic/2017-2018/CS 229/Project/old npy/checkpoints/'
-    # if not os.path.exists(save_dir):
-    #     os.makedirs(save_dir)
-    # save_path = os.path.join(save_dir, 'epoch_validation')
-    # save_file = 'model.ckpt'
-
+    save_path = saver.save(sess, "/tmp/model.ckpt")
     # start tensorflow session
     sess = tf.Session(config=config)
     sess.run(tf.global_variables_initializer())
-    #saver.save(sess, './3_model')
-    save_path = saver.save(sess, "/tmp/model.ckpt")
-
-    print( tf.trainable_variables())
-    merged = tf.summary.merge_all()
-    #summary_writer
-    max_value = originalMatrix.shape[1] - originalMatrix.shape[1] % 5000 - 1
-    '''
-    for i in range(18):
-        left = 5000*i
-        right = 5000*(i+1)
-        channelMatrix = originalMatrix[:,left:right,:]
-        music_input = matrix_to_input(channelMatrix)
-        _,_, loss,accuracy = sess.run([merged,model.train_op, model.loss, model.accuracy], feed_dict={placeholder['music_input']: music_input})
     
-        print(loss)
-        print(accuracy)
-    '''
-    # summary_writer = tf.summary.FileWriter('/Users/yuxing/Desktop/Stanford/Academic/2017-2018/CS\ 229/plots/1', sess.graph)
-    #dev_writer = tf.summary.FileWriter('/Users/yuxing/Desktop/Stanford/Academic/2017-2018/CS\ 229/plots/2', sess.graph) 
-   
-    num_epoch = 10
-    initial_flag_train = True
-    # initial_flag_dev = True
-
-    step = 0
-
-    print(music_input.shape[2])
-
-    # min_loss = 2
+    #print( tf.trainable_variables())
+    merged = tf.summary.merge_all()
+    max_value = originalMatrix.shape[1] - originalMatrix.shape[1] % 5000 - 1
+    #print(music_input.shape[2])
 
     # first state
     state = np.zeros((music_input.shape[0], state_dim))
-
+    
+    ############################ training set ###########################3
     for epoch in range(num_epoch):
         print ('++++++++++++++++')
         print ("Current epoch: ", epoch)
@@ -158,12 +112,10 @@ def main(argv=None):
             # update i for next step
             i += seq_len
     
-            # _,_, loss, grad, pred, input = sess.run([merged, model.train_op, model.loss, model.grad,
-            #         model.pred, model.music_input], feed_dict={placeholder['music_input']: batch_input})
+            # run session 
             val,_, loss, grad, input, initial_state = sess.run([merged, model.train_op, model.loss, model.grad,
                     model.music_input, model.initial_state], feed_dict={placeholder['music_input']: batch_input,
                                                                 placeholder['state_input']: state})
-            
 
             #summary_writer.add_summary(val,step)
             # step += 1
@@ -175,14 +127,15 @@ def main(argv=None):
                 #print(initial_state)
                 curr_grad = LA.norm(grad)
                 print(curr_grad)
-        # saver.save(sess, './3_model')
-        save_path = saver.save(sess, "/tmp/model.ckpt")
-
+        
+        save_path = saver.save(sess, "/tmp/model.ckpt")  # save the path for generation
+        
+        #################### dev set loss calculation ###########################
         
         print ('%-------%')
         total_loss = 0
-        i = randint(0, 100)
-        for iter in range(2000):
+        i = randint(0, 100)    # choose a random start point
+        for iter in range(2000):  # feed a sequence of length seq into the placeholder
             if i > dev_input.shape[1] - seq_len:
                 print ("hiiiii")
                 break
@@ -204,64 +157,10 @@ def main(argv=None):
                 #dev_writer.add_summary(val,epoch+1)
                 print ("avg_loss is: ", avg_loss)
         
-
-        # print ('%-------%')
-        # for iter in range(200000):
-        #     i = randint(0, dev_set.shape[1] - seq_len)
-        #     batch_input = dev_input[:,i:i+seq_len,:]
-        #     # _,_, loss, grad, pred, input = sess.run([merged, model.train_op, model.loss, model.grad,
-        #     #         model.pred, model.music_input], feed_dict={placeholder['music_input']: batch_input})
-        #     val,_, loss, grad, input = sess.run([merged, model.train_op, model.loss, model.grad,
-        #             model.music_input], feed_dict={placeholder['music_input']: batch_input})
-        #     if iter % 1000 == 0:
-        #         if initial_flag_dev:
-        #             dev_writer.add_summary(val,0)
-        #             initial_flag_dev = False
-        #         print('Iter ', iter, ': loss=', loss)
-        #         curr_grad = LA.norm(grad)
-        #         print(curr_grad)
-        #         if curr_grad < 1e-6:
-        #             dev_writer.add_summary(val,epoch+1)
-        #             break
-
-            #print(accuracy)
         print("Loss for epoch %d = %f" % (epoch,loss)) #use this if we wanna generate a plot of loss vs. epoch
-    # print (min_loss)
-    # save_path = saver.save(sess, "/tmp/model.ckpt")
+    
     print("Done Training")
-    '''
-    ## try music generation:
     
-    ## initialization :
-    state_dim=128   
-    batch_size = music_input.shape[0]
-    time_range = music_input.shape[1]
-    note_input_dim = music_input.shape[2]
-    note_output_dim = music_input.shape[2]
-    print()
-    new_state_gen = np.zeros([1,2*state_dim])
-    start_vec = tf.placeholder(tf.float32,[1,1,note_input_dim])
-    in_state = tf.placeholder(tf.float32,[1,2*state_dim])
-    output, new_state = tf.nn.dynamic_rnn(cell=lstm,inputs = start_vec,initial_state=in_state,dtype=tf.float32)
-    output = tf.reshape(output,[1,state_dim])
-    out_logits = tf.matmul(output,W)+ b
-    
-    seq = [6] #the initial sequence we feed the LSTM
-    if len(seq) > 1:
-        x_init = np.reshape([make_feature_vec(i) for i in seq[:-1]],[1,len(seq)-1,note_input_dim])
-        new_state_gen = session.run(state,feed_dict = {x:x_init})
-    start = np.reshape(make_feature_vec(seq[-1]),[1,1,note_input_dim])
-    for i in range(20):
-        new_out_logits, new_state_gen = session.run([out_logits,new_state], feed_dict={start_vec:start,in_state:new_state_gen})
-        index = int(tf.argmax(new_out_logits, 1).eval())
-        seq.append(index)
-        start = np.reshape(make_feature_vec(index),[1,1,note_input_dim])
-
-    current = seq
-    print('Final: {}'.format(current))
-    current_converted = [num_to_vec[current[i]] for i in range(len(current))]
-    print('Final converted: {}'.format(current_converted))
-    '''
     
 if __name__ == "__main__":
     tf.app.run()
